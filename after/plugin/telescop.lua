7fdd524c0cd876b1a5f7dc5ae797a4b2c20eaf24
abhayfaldu <abhayfaldu1922@gmail.com>
2023-04-05 19:39:28 +0530

    removed unnessesory lines of comments

--- after/plugin/telescop.lua
+++ after/plugin/telescop.lua
@@ -3,19 +3,19 @@ local telescope_setup, telescope = pcall(require, "telescope")
 if not telescope_setup then
 	return
 end
---
+
 -- import telescope actions safely
 local actions_setup, actions = pcall(require, "telescope.actions")
 if not actions_setup then
 	return
 end
---
+
 -- import telescope-ui-select safely
 local themes_setup, themes = pcall(require, "telescope.themes")
 if not themes_setup then
 	return
 end
---
+
 -- configure telescope
 telescope.setup({
 	-- configure custom mappings
@@ -34,6 +34,6 @@ telescope.setup({
 		},
 	},
 })
---
+
 telescope.load_extension("fzf")
 telescope.load_extension("ui-select")
--- lua/abhay/set.lua
+++ lua/abhay/set.lua
@@ -1,55 +1,55 @@
 local opt = vim.opt -- for consiseness
---
+
 opt.guicursor = ""
---
+
 -- line numbers
 opt.number = true -- show relative line numbers
 opt.relativenumber = true -- shows absolute line number on cursor line (when relative number is on)
---
+
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.softtabstop = 2 -- 2 spaces for indent width
 opt.shiftwidth = 2 -- expand tab to spaces
 opt.expandtab = true -- copy indent from current line when starting new one
---
+
 opt.autoindent = true
 opt.smartindent = true
---
+
 -- line wrapping
 opt.wrap = false -- disable line wrapping
---
+
 opt.swapfile = false
 opt.backup = false
 opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 opt.undofile = true
---
+
 -- search settings
 opt.hlsearch = false
 opt.incsearch = true
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sen
---
+
 opt.termguicolors = true
---
+
 opt.scrolloff = 8
 opt.signcolumn = "yes"
 opt.isfname:append("@-@")
---
+
 opt.updatetime = 50
---
--- opt.colorcolumn = "100"
---
+
+opt.colorcolumn = "100"
+
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
 opt.signcolumn = "yes"
 vim.g.nightflyTransparent = true
---
+
 opt.backspace = "indent,eol,start"
---
+
 -- clipboard
 -- opt.clipboard:append("unnamedplus") -- use system clipboard as default register
---
+
 -- split windows
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
---
+
 -- opt.iskeyword:append("-") -- consider string-string as whole word
