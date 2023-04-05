720e9f39c51e68d53cb1283320089c89ce22a7fa
abhayfaldu <abhayfaldu1922@gmail.com>
2023-04-05 13:38:37 +0530

    verison_2 added with some extra functionality and some other changes

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,14 +1,9 @@
-require('rose-pine').setup({
-    disable_background = true
-})
-
-function ColorMyPencils(color) 
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+-- set colorscheme to nightfly with protected call
+-- in case it isn't installed
+local status, _ = pcall(vim.cmd, "colorscheme nightfly")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+if not status then
+  print("Colorscheme not found!") -- print error if colorscheme not installed
+  return
 end
-
-ColorMyPencils()
--- lua/abhay/set.lua
+++ lua/abhay/set.lua
@@ -1,32 +1,55 @@
-vim.opt.guicursor = ""
-
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
-
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "100"
-
+local opt = vim.opt -- for consiseness
+--
+opt.guicursor = ""
+--
+-- line numbers
+opt.number = true -- show relative line numbers
+opt.relativenumber = true -- shows absolute line number on cursor line (when relative number is on)
+--
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.softtabstop = 2 -- 2 spaces for indent width
+opt.shiftwidth = 2 -- expand tab to spaces
+opt.expandtab = true -- copy indent from current line when starting new one
+--
+opt.autoindent = true
+opt.smartindent = true
+--
+-- line wrapping
+opt.wrap = false -- disable line wrapping
+--
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+--
+-- search settings
+opt.hlsearch = false
+opt.incsearch = true
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sen
+--
+opt.termguicolors = true
+--
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+opt.isfname:append("@-@")
+--
+opt.updatetime = 50
+--
+-- opt.colorcolumn = "100"
+--
+-- cursor line
+opt.cursorline = true -- highlight the current cursor line
+opt.signcolumn = "yes"
+vim.g.nightflyTransparent = true
+--
+opt.backspace = "indent,eol,start"
+--
+-- clipboard
+-- opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+--
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+--
+-- opt.iskeyword:append("-") -- consider string-string as whole word
