ccece024c108cca710dceb8cb0a02d1156aeb360
abhayfaldu <abhayfaldu1922@gmail.com>
2023-04-10 18:04:20 +0530

    Made some chnages in toggleterm

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -4,16 +4,12 @@ if not status_ok then
 end
 
 toggleterm.setup({
-	size = 20,
+	size = 15,
 	open_mapping = [[<c-t>]],
 	hide_numbers = true,
-	shade_filetypes = {},
-	shade_terminals = true,
-	shading_factor = 2,
 	start_in_insert = true,
-	insert_mappings = true,
 	persist_size = true,
-	direction = "vertical",
+	direction = "horizontal",
 	close_on_exit = true,
 	shell = vim.o.shell,
 	float_opts = {
@@ -28,8 +24,6 @@ toggleterm.setup({
 
 function _G.set_terminal_keymaps()
 	local opts = { noremap = true }
-	vim.api.nvim_buf_set_keymap(0, "t", "<ESC>", "<C-t>", opts)
-	--vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-t><C-n>]], opts)
 	vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", "<C-t><C-n><C-W>h", opts)
 	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", "<C-t><C-n><C-W>j", opts)
 	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", "<C-t><C-n><C-W>k", opts)
